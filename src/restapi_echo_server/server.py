294e6b9703b298c68a5f60667c7e44ef974b1bfe
Chris Browet <cbro@semperpax.com>
2023-08-02 15:09:30 +0200

    Handle json specifically

--- src/restapi_echo_server/server.py
+++ src/restapi_echo_server/server.py
@@ -8,10 +8,24 @@ logger = logging.getLogger(__name__)
 
 
 class Handler(http_server.BaseHTTPRequestHandler):
+
+    def __init__(self, *args):
+        self.body_json = None
+        self.body_raw = None
+
+        http_server.BaseHTTPRequestHandler.__init__(self, *args)
+
     def _handle(self):
         parsed_path = parse.urlparse(self.path)
 
         content_length = int(self.headers.get('content-length', 0))
+
+        self.body_raw = self.rfile.read(content_length).decode('utf-8')
+        if "content-type" in self.headers and self.headers['content-type'].lower() == 'application/json' and content_length > 0:
+            try:
+                self.body_json = json.loads(self.body_raw)
+            except:
+                self.body_json = None
         body = self.rfile.read(content_length).decode('utf-8')
         
         response = {
@@ -19,7 +33,7 @@ class Handler(http_server.BaseHTTPRequestHandler):
             "path": parsed_path.path,
             "query": parse.parse_qs(parsed_path.query),
             "headers": dict(self.headers),
-            "body": body,
+            "body": self.body_json if self.body_json is not None else self.body_raw,
             "received_at": datetime.datetime.utcnow().isoformat(),
             "client": {
                 "host": self.client_address[0],
@@ -37,6 +51,8 @@ class Handler(http_server.BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(response).encode('utf-8'))
+
+        logger.info(f"{json.dumps(response)}")
 
     def do_GET(self):
         self._handle()
